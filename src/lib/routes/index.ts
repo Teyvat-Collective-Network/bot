b1538692454b57bf447452cb3e0564cca05247f7
hyper-neutrino <alex.yj.liao@gmail.com>
2023-09-25 17:44:24 -0400

    add tag fetch route

--- src/lib/routes/index.ts
+++ src/lib/routes/index.ts
@@ -3,23 +3,38 @@ import { App } from "../../index.js";
 import bot from "../bot.js";
 
 export default (app: App) =>
-    app.get(
-        "/invite/:code",
-        async ({ params: { code } }) => {
-            try {
-                const invite = await bot.fetchInvite(code);
+    app
+        .get(
+            "/invite/:code",
+            async ({ params: { code } }) => {
+                try {
+                    const invite = await bot.fetchInvite(code);
 
-                return {
-                    code: invite.code,
-                    id: invite.guild?.id,
-                    vanity: invite.guild?.vanityURLCode === invite.code,
-                    target: !!(invite.targetApplication || invite.targetUser),
-                };
-            } catch {
-                return new Response("Invite not found.", { status: 404 });
-            }
-        },
-        {
-            params: t.Object({ code: t.String() }),
-        },
-    );
+                    return {
+                        code: invite.code,
+                        guild: invite.guild,
+                        vanity: invite.guild?.vanityURLCode === invite.code,
+                        target: !!(invite.targetApplication || invite.targetUser),
+                    };
+                } catch {
+                    return new Response("Invite not found.", { status: 404 });
+                }
+            },
+            {
+                params: t.Object({ code: t.String() }),
+            },
+        )
+        .get(
+            "/tag/:id",
+            async ({ params: { id } }) => {
+                try {
+                    const user = await bot.users.fetch(id);
+                    return JSON.stringify(user.tag);
+                } catch {
+                    return new Response("User not found.", { status: 404 });
+                }
+            },
+            {
+                params: t.Object({ id: t.String({ pattern: "^\\d{17,20}$", default: "1234567890987654321" }) }),
+            },
+        );
