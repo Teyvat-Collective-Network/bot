1695de62a7f63884b185fd97ccb22d2db9761b25
hyper-neutrino <alex.yj.liao@gmail.com>
2023-10-15 13:37:11 -0400

    ensure observer for /hq invite

--- src/commands/hq/invite.ts
+++ src/commands/hq/invite.ts
@@ -1,5 +1,6 @@
 import { ChatInputCommandInteraction } from "discord.js";
 import { channels, hq } from "../../lib/bot.js";
+import { ensureObserver } from "../../lib/permissions.js";
 import { CommandData } from "../../lib/types.js";
 
 export const command: CommandData = {
@@ -10,6 +11,7 @@ export const command: CommandData = {
 
 export default async function (cmd: ChatInputCommandInteraction) {
     await cmd.deferReply({ ephemeral: true });
+    await ensureObserver(cmd);
 
     const invite = await hq.invites.create(channels.INFO_AND_RULES, { maxAge: 7 * 24 * 60 * 60, maxUses: 1, unique: true });
     await channels.BOT_LOGS.send(`${cmd.user} created a 1-use 1-week invite.`).catch();
