822cbd43c8fa99e0d2e0b004017ee41ffd8cb43a
hyper-neutrino <alex.yj.liao@gmail.com>
2023-10-22 03:14:58 -0400

    typo

--- src/commands/hq/partner-list.ts
+++ src/commands/hq/partner-list.ts
@@ -35,7 +35,7 @@ export default async function (cmd: ChatInputCommandInteraction) {
             })),
         });
 
-    await cmd.channel!.send(`**Autosync Guide: ${Bun.env.WEBSITE}/info/partner-list#autosync`);
+    await cmd.channel!.send(`**Autosync Guide**: ${Bun.env.WEBSITE}/info/partner-list#autosync`);
 
     return "The long-form partner list has been generated.";
 }
