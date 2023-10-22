03cbefafe71820efae486f0525605d5344d90055
hyper-neutrino <alex.yj.liao@gmail.com>
2023-10-22 03:12:36 -0400

    add long-form partner list generator

---
+++ src/commands/hq/partner-list.ts
@@ -0,0 +1,40 @@
+import { ChatInputCommandInteraction } from "discord.js";
+import api, { getToken } from "../../lib/api.js";
+import { ensureObserver } from "../../lib/permissions.js";
+import { success } from "../../lib/responses.js";
+import { Attribute, Character, CommandData, TCNGuild } from "../../lib/types.js";
+
+export const command: CommandData = {
+    key: "partner-list",
+    description: "generate the long-form partner list",
+};
+
+export default async function (cmd: ChatInputCommandInteraction) {
+    await cmd.reply(success("Generating, please be patient..."));
+    await cmd.deferReply({ ephemeral: true });
+    await ensureObserver(cmd);
+
+    const token = await getToken(cmd);
+
+    const guilds: TCNGuild[] = await api(token, `GET /guilds`);
+    const characters: Record<string, Character> = Object.fromEntries((await api(token, `GET /characters`)).map((x: Character) => [x.id, x]));
+    const attributes: Record<string, Record<string, Attribute>> = await api(token, `GET /attributes`);
+
+    while (guilds.length > 0)
+        await cmd.channel!.send({
+            embeds: guilds.splice(0, 10).map((guild) => ({
+                title: guild.name,
+                description: `${["element", "weapon", "region"]
+                    .map((x) => attributes[x][characters[guild.mascot].attributes[x] ?? ""])
+                    .filter((x) => x)
+                    .join(" ")} ${characters[guild.mascot].name}${
+                    characters[guild.mascot].short ? ` (${characters[guild.mascot].short})` : ""
+                }\n\n**Owner:** <@${guild.owner}>${guild.advisor ? `\n**Advisor**: <@${guild.advisor}>` : ""}`,
+                color: 0x2b2d31,
+                thumbnail: { url: `${Bun.env.WEBSITE}/files/${guild.mascot}.png` },
+                footer: { text: guild.id },
+            })),
+        });
+
+    return "The long-form partner list has been generated.";
+}
